aa739810fc21ae8d70a10017a2f02da54fdceb48
artoo - Diego V <scriem@gmail.com>
2020-03-09 21:58:40 +0000

    the count of cases were totals and not daily count

--- Italy.R
+++ Italy.R
@@ -16,12 +16,25 @@ subset<-data[which(data$Country.Region == "Italy"),]
 library(reshape2)
 
 long <- melt(subset, id.vars = c("Country.Region"))
-colnames(long)<- c("country","date","count")
+colnames(long)<- c("country","date","total")
 
+###CAREFUL IN THIS DATASET SOME DATA IS TOTAL AND NOT DAILY
 
+#We must calculate daily increase and model each daily measure instead
+# modelling the total in order to avoid adding up the error
 long$day<-1:length(long$date)
 
-long<-long[29:47,]
+i<-1
+long$count<-1
+# calculate  the difference in scores to model the daily increase and not the total count
+while (i <= length(long$day)){
+  if(i!=1){
+    long$count[i]<-long$total[i]-long$total[i-1]
+  }else{
+    long$count[i]<-long$total[i]
+  }
+  i<-i+1
+}
 
 
 plot(long$day,long$count)
@@ -30,7 +43,10 @@ plot(long$day,long$count)
 #                  geom_point(size=2, shape=1)
 #
 
-
+#########################################
+# CURRENTLY (09/03/20) the model fails because the number of measures is too small
+# to estimate the start parameters. Below there is a snippet to "eyeball" the start parameters
+######################################### 
 
 #find the parameters for the equation
 SS<-getInitial(count~SSlogis(day,alpha,xmid,scale),data=data.frame(count=long$count,day=long$day))
@@ -49,3 +65,20 @@ summary(m)
 cor(long$count,predict(m))
 
 lines(long$day,predict(m),col="red",lty=2,lwd=3)
+
+
+######################################### estimation with eyeballed parameteres
+
+a_start<-33 #param a is the y value when x=0
+b_start<-2*log(2)/a_start #b is the decay rate
+
+m<-nls(count~a*exp(b*day),start=list(a=a_start,b=b_start), data = long)
+
+summary(m)
+
+#get some estimation of goodness of fit
+cor(long$count,predict(m))
+
+
+plot(long$day,long$count)
+lines(long$day,predict(m),col="red",lty=3,lwd=2)
