59456f9023ddd0380e9d6e534b3766af47e11d04
artoo - Diego V <scriem@gmail.com>
2020-03-10 12:22:29 +0000

    added population and daily increase
    Polation should go into a logistic distribution. model will fail is the growth is only exponential. Try the commented code if the distribution is still just exponential

--- South-Korea.R
+++ South-Korea.R
@@ -1,32 +1,89 @@
-################################
 
-#Online Data South Korea
+######################################################################################
 
-###############################
+###                                 Online Data South Korea
+
+#####################################################################################
+
 
 #https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
 
+#####################
+country = "South Korea" 
+####################
 
 
 data<-read.csv(file = "/home/diego/Windows/time_series_19-covid-Confirmed.csv",header = TRUE,sep = ",")
-data<-data %>% select(2,5:51)
 
-subset<-data[which(data$Country.Region == "South Korea"),]
+data<-data %>% select(2,5:ncol(data))
+
+subset<-data[which(data$Country.Region == country),]
 
 library(reshape2)
 
 long <- melt(subset, id.vars = c("Country.Region"))
-colnames(long)<- c("country","date","count")
+
+
+############################################# 
+####################
+#################### observed daily count plot: It should not look exponential if measures are working
+####################
+#############################################
+colnames(long)<- c("country","date","total")
+
 
 long$day<-1:length(long$date)
+
+long$count <- 1 #initialize a count variable
+i<-1
+
+# calculate  the difference in scores to model the daily increase and not the total count
+while (i <= length(long$day)){
+  if(long$total[i] > 1){
+    long$count[i]<-long$total[i]-long$total[i-1]
+  }else{
+    long$count[i]<-long$total[i]
+  }
+  i<-i+1
+}
 
 
 plot(long$day,long$count)
 
-#Long %>% ggplot(aes(x = day, y = count))+
-#                  geom_point(size=2, shape=1)
-#
+# uncomment this if the distribution is just exponential
+#a_start<-33 #param a is the y value when x=0
+#b_start<-2*log(2)/a_start #b is the decay rate
 
+#m<-nls(count~a*exp(b*day),start=list(a=a_start,b=b_start), data = long)
+
+#summary(m)
+
+#get some estimation of goodness of fit
+#cor(long$count,predict(m))
+
+
+#plot(long$day,long$count)
+#lines(long$day,predict(m),col="red",lty=3,lwd=2)
+
+
+############################################# 
+####################
+#################### Total count plot: It should compile if the data presents a logistic curve
+####################
+#############################################
+
+subset<-data[which(data$Country.Region == country),]
+
+library(reshape2)
+
+long <- melt(subset, id.vars = c("Country.Region"))
+
+long<-long[1:nrow(long),]
+
+colnames(long)<- c("country","date","count")
+
+
+long$day<-1:nrow(long)
 
 
 #find the parameters for the equation
@@ -45,4 +102,24 @@ summary(m)
 #get some estimation of goodness of fit
 cor(long$count,predict(m))
 
+plot(long$day,long$count)
 lines(long$day,predict(m),col="red",lty=2,lwd=3)
+
+
+# uncomment this if the distribution is just exponential
+#a_start<-33 #param a is the y value when x=0
+#b_start<-2*log(2)/a_start #b is the decay rate
+
+#m<-nls(count~a*exp(b*day),start=list(a=a_start,b=b_start), data = long)
+
+#summary(m)
+
+#get some estimation of goodness of fit
+#cor(long$count,predict(m))
+
+
+#plot(long$day,long$count)
+#lines(long$day,predict(m),col="red",lty=3,lwd=2)
+
+
+
